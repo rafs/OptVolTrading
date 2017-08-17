f5c189786bc754a88923c1ce83c485c9e5c3220e
YuJun <yujun1214@gmail.com>
2017-08-17 22:56:17 +0800

    20170817

--- OptVolTrendTrading.py
+++ OptVolTrendTrading.py
@@ -227,31 +227,93 @@ class CVolTrendTradingStrategy(object):
         atm_ratio, otm_ratio = tuple(int(n) for n in self.spread_ratio.split(':'))
         return atm_ratio, otm_ratio
 
-    def do_position(self, trading_datetime, opt_type, capital):
+    def do_position(self, trading_beg_datetime, trading_min_num, opt_type, capital):
         """
         进行建仓操作
-        :param trading_datetime: 交易时间
+        :param trading_beg_datetime: 建仓开始时的交易时间
+        :param trading_min_num: 建仓所需的分钟数，整型
         :param opt_type: 期权类型，Call=认购比率，Put=认沽比率
         :param capital: 建仓所采用的资本金（多头使用的金额+空头占用的保证金）
-        :return:
+        :return: 建仓完成时的交易时间，=最后一笔建仓时间的后一分钟
         """
-        # 1.取得平值期权、虚值一档的期权
-        fUnderlying_price = self.underlying_quote_1min.ix[trading_datetime, 'close']
-        atm_opt, otm_opt = self.get_ratiospread_opts(fUnderlying_price, opt_type)
+        # 1.取得平值期权、虚值一档的期权，并设置比率价差的代码
+        funderlying_price = self.underlying_quote_1min.ix[trading_beg_datetime, 'close']
+        atm_opt, otm_opt = self.get_ratiospread_opts(funderlying_price, opt_type)
+        if opt_type == 'Call':
+            self.call_ratio = (atm_opt.code, otm_opt.code)
+        else:
+            self.put_ratio = (atm_opt.code, otm_opt.code)
+
         # 2.取得平值期权、虚值期权的单位数量
         atm_ratio, otm_ratio = self.get_spread_ratio()
         # 3.计算平值期权、虚值期权的建仓数量
-        funit_cost = atm_opt.quote_1min.ix[trading_datetime, 'close'] * atm_opt.multiplier * atm_ratio + otm_opt.margin * otm_ratio
-        num_of_units = int(round(capital / funit_cost, 0))
+        funit_cost = atm_opt.quote_1min.ix[trading_beg_datetime, 'close'] * atm_opt.multiplier * atm_ratio + otm_opt.margin * otm_ratio
+        num_of_units = int(round(capital / trading_min_num / funit_cost, 0))
         atm_vol, otm_vol = (atm_ratio * num_of_units, otm_ratio * num_of_units)
-        # 4.生成交易清单
+        # 4.在接下来的trading_min_num分钟里建仓，每分钟平值、虚值的建仓量分别为atm_vol和otm_vol
         trade_datas = []
-        atm_price = atm_opt.quote_1min.ix[trading_datetime, 'close']
-        trade_datas.append(COptTradeData(atm_opt.code, 'buy', 'open', atm_price, atm_vol,
-                                         atm_price * atm_vol * atm_opt.multiplier, atm_vol * self.commission_per_unit,
-                                         trading_datetime, atm_opt))
-        otm_price = otm_opt.quote_1min.ix[trading_datetime, 'close']
-        trade_datas.append(COptTradeData(otm_opt.code, 'sell', 'open', otm_price, otm_vol,
-                                         otm_price * otm_vol * otm_opt.multiplier, 0.0, trading_datetime, otm_opt))
+        for min_num in range(trading_min_num):
+            trading_datetime = trading_beg_datetime + datetime.timedelta(minutes=min_num)
+            atm_price = atm_opt.quote_1min.ix[trading_datetime, 'close']
+            trade_datas.append(COptTradeData(atm_opt.code, 'buy', 'open', atm_price, atm_vol,
+                                             atm_price * atm_vol * atm_opt.multiplier, atm_vol * self.commission_per_unit,
+                                             trading_datetime, atm_opt))
+            otm_price = otm_opt.quote_1min.ix[trading_datetime, 'close']
+            trade_datas.append(COptTradeData(otm_opt.code, 'sell', 'open', otm_price, otm_vol,
+                                             otm_price * otm_vol * otm_opt.multiplier, 0.0, trading_datetime, otm_opt))
         # 5.更新持仓数据
         self.opt_holdings.update_holdings(trade_datas)
+        # 6.返回建仓完成时的交易时间，=最后一笔建仓时间的后一分钟
+        return trading_beg_datetime + datetime.timedelta(minutes=trading_min_num)
+
+    def do_liquidation(self, trading_beg_datetime, trading_min_num, opt_type):
+        """
+        进行平仓操作
+        :param trading_beg_datetime: 平仓开始时的交易时间
+        :param trading_min_num: 平仓所需的分钟数，整型
+        :param opt_type: 期权类型，Call=认购比率，Put=认沽比率
+        :return: 平仓完成时的交易时间，=最后一笔平仓时间的后一分钟
+        """
+        # 1.取得平值期权、虚值期权的代码
+        if opt_type == 'Call':
+            atm_code, otm_code = self.call_ratio
+        else:
+            atm_code, otm_code = self.put_ratio
+        # 2.根据平仓的分钟数，计算每分钟平仓的数量
+        atm_vol = int(self.opt_holdings.holdings[atm_code].holdingvol / trading_min_num + 0.5)
+        otm_vol = int(self.opt_holdings.holdings[otm_code].holdingvol / trading_min_num + 0.5)
+        # 3.在接下来的trading_min_num分钟里平仓，每分钟平仓平值、虚值的数量分别为atm_vol和opt_vol
+        for min_num in range(trading_min_num):
+            trade_datas = []
+            trading_datetime = trading_beg_datetime + datetime.timedelta(minutes=min_num)
+            atm_price = self.opts_data[atm_code].quote_1min.ix[trading_datetime, 'close']
+            otm_price = self.opts_data[otm_code].quote_1min.ix[trading_datetime, 'close']
+            # 如果时间超过14:59:00，将剩余的全部平仓
+            if trading_datetime.time() >= datetime.time(14, 59, 0):
+                atm_vol = self.opt_holdings.holdings[atm_code].holdingvol
+                if atm_vol > 0:
+                    trade_datas.append(COptTradeData(atm_code, 'sell', 'close', atm_price, atm_vol, atm_price * atm_vol,
+                                                     atm_vol * self.commission_per_unit, trading_datetime,
+                                                     self.opts_data[atm_code]))
+                otm_vol = self.opt_holdings.holdings[otm_code].holdingvol
+                if otm_vol > 0:
+                    trade_datas.append(COptTradeData(otm_code, 'buy', 'close', otm_price, otm_vol, otm_price * otm_vol,
+                                                     otm_vol * self.commission_per_unit, trading_datetime,
+                                                     self.opts_data[otm_code]))
+            else:
+                if atm_vol > self.opt_holdings.holdings[atm_code].holdingvol:
+                    atm_vol = self.opt_holdings.holdings[atm_code].holdingvol
+                if otm_vol > self.opt_holdings.holdings[otm_code].holdingvol:
+                    otm_vol = self.opt_holdings.holdings[otm_code].holdingvol
+                if atm_vol > 0:
+                    trade_datas.append(COptTradeData(atm_code, 'sell', 'close', atm_price, atm_vol, atm_price * atm_vol,
+                                                     atm_vol * self.commission_per_unit, trading_datetime,
+                                                     self.opts_data[atm_code]))
+                if otm_vol > 0:
+                    trade_datas.append(COptTradeData(otm_code, 'buy', 'close', otm_price, otm_vol, otm_price * otm_vol,
+                                                     otm_vol * self.commission_per_unit, trading_datetime,
+                                                     self.opts_data[otm_code]))
+            # 更新持仓数据
+            self.opt_holdings.update_holdings(trade_datas)
+        # 返回平仓完成时的交易时间，=最后一笔平仓时间的后一分钟
+        return trading_beg_datetime + datetime.timedelta(minutes=trading_min_num)
